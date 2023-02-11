17766e83271dc6576e60d28174157667c418866c
t-onyango <t.onyango@alustudent.com>
2023-02-11 18:46:18 +0200

    functions in dart

--- functions/main.dart
+++ functions/main.dart
@@ -1,33 +1,67 @@
 void main(){
-  //  function invocations
-  print(sayHi());
-  print(add(20, 40));
-  print(details());
-  print(subNum(19));
-  //Using anonymous function to print out elements in a list
-  List myGrade = [85, 94, 75, 83];
-  myGrade.forEach((element) {print(element);});
-  stuff(details());
-
-
-
+  print(addTwo(30, 40));
+  print(subtractTwo(70, 20));
+  print(multiplyTwo(5, 20));
+  print(divideTwo(4, 2));
+  print(stringLength(''));
+  print(getFirstElement());
 
 }
-//function with return type
-int add(int x, int y){
+/*
+Task 1
+Write a function called addTwo that takes two numbers as
+ arguments and returns the sum of those two numbers.
+*/
+int addTwo(int x, int y){
   return x + y;
 }
-//function without parameters
-String details(){
-  return'Got your deatils';
-}
-//function with mandatory parameters
-void stuff(String details){
-  print(details);
-}
-//An arrow function with return type double
-double subNum(double y) => y - 1;
 
-String sayHi(){
-  return 'Hello there!';
+/*
+ Task 2
+Write a function called subtractTwo that takes two 
+numbers as arguments and returns the difference of those 
+two numbers.
+*/
+double subtractTwo(double x, double y){
+  return x - y;
+}
+
+/*
+Task 3
+Write a function called multiplyTwo that takes two numbers 
+as arguments and returns the product of those two numbers.
+*/
+
+double multiplyTwo(double num1, double num2){
+  return num1 * num2;
+}
+
+/*
+Task 4
+Write a function called divideTwo that takes two numbers 
+as arguments and returns the quotient of those two numbers.
+*/
+double divideTwo(double num1, double num2){
+  return num1 / num2;
+}
+
+/*
+Task 5
+Write a function called stringLength that takes an argument 
+of type String and returns the length of that string.
+*/
+int stringLength(String name){
+  name = 'Topister';
+  int final_len = name.length;
+  return final_len;
+}
+
+/*
+Task 6
+Write a function called getFirstElement that takes a list 
+as an argument and returns the first element of that list.
+*/
+
+String getFirstElement([String fname = 'Topister', String lname = 'Nandera']){
+  return lname;
 }
